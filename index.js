c77e97a3be0239c2834e83a5a92dba138d176934
Paulo Hernane <paulo@mobixtec.com>
2022-09-09 15:46:20 -0300

    fix build

--- index.js
+++ index.js
@@ -12,23 +12,33 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send({ message: 'up' });
+  try {
+    res.send({ message: 'up' });
+  } catch (error) {
+    console.error(error);
+    res.send({ error });
+  }
 });
 
 app.post('/token', (req, res) => {
-  const { identity, roomName } = req.body;
+  try {
+    const { identity, roomName } = req.body;
 
-  const videoGrant = new twilio.jwt.AccessToken.VideoGrant({
-    room: roomName,
-  });
+    const videoGrant = new twilio.jwt.AccessToken.VideoGrant({
+      room: roomName,
+    });
 
-  const token = new twilio.jwt.AccessToken(account_sid, api_key, api_secret);
+    const token = new twilio.jwt.AccessToken(account_sid, api_key, api_secret);
 
-  token.addGrant(videoGrant);
+    token.addGrant(videoGrant);
 
-  token.identity = identity;
+    token.identity = identity;
 
-  res.send({ token: token.toJwt() });
+    res.send({ token: token.toJwt() });
+  } catch (error) {
+    console.error(error);
+    res.send({ error });
+  }
 });
 
 app.listen(process.env.PORT ?? 3333, () => {
